d5b6a2369155b0a538cdb98515f97c38d8c50d20
yggdrasil75 <cblackburn7557@gmail.com>
2024-10-04 17:56:32 -0400

    added better DDS support via setting compression type so games that require bc3 (stellaris) can be supported better while games that prefer bc5 (skyrim) can benefit. since this is not really something for general extended it is now its own node. dds in extendedsave node is for uncompressed.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .extended_image_formats import ExtendedSaveImage
+from .extended_image_formats import ExtendedSaveImage, DDSSaveImage
 
 
 # Set the web directory, any .js file in that directory will be loaded by the frontend as a frontend extension
@@ -6,7 +6,7 @@ WEB_DIRECTORY = "./web/"
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
-NODE_CLASS_MAPPINGS = {"ExtendedSaveImage": ExtendedSaveImage}
+NODE_CLASS_MAPPINGS = {"ExtendedSaveImage": ExtendedSaveImage, "DDSSaveImage": DDSSaveImage}
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
-NODE_DISPLAY_NAME_MAPPINGS = {"ExtendedSaveImage": "Extended Save"}
+NODE_DISPLAY_NAME_MAPPINGS = {"ExtendedSaveImage": "Extended Save", "DDSSaveImage": "Save DDS for games"}
